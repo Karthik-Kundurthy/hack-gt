5305dc823784322e67aba5ce8507095976245dd8
Samyak <samyakjainbvs@gmail.com>
2021-10-22 22:04:54 -0400

    Flow

--- carpool/lib/main.dart
+++ carpool/lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:carpool/screens/login_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Carpool'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"),
-          ),
-        ),
-        backgroundColor: Colors.blueGrey[500],
-      ),
-    ),
-  );
+void main() => runApp(Carpool());
+
+class Carpool extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 'login_screen',
+      routes: {
+        'login_screen': (context) => LoginScreen(),
+      },
+    );
+  }
 }
